47a01a8fce95fb763547916d9a00b5851fc7821d
Alexandre CUER <alexandre.cuer@cerema.fr>
2020-07-04 17:52:37 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 print("hello")
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import numpy as np
+
+T=np.ones(500)*12
+
+plt.subplot(111)
+plt.plot(T)
+plt.show()
+plt.savefig("myfig")
