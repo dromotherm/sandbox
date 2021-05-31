7577ce4d1f38aee686b826c312e215d937a097aa
Alexandre CUER <alexandre.cuer@cerema.fr>
2021-05-31 13:54:24 +0200

    Create mosquitto.sh

---
+++ MQTT/mosquitto.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "-------------------------------------------------------------"
+echo "Mosquitto Server installation and configuration"
+echo "-------------------------------------------------------------"
+sudo apt-get install -y mosquitto
+sudo apt-get install -y libmosquitto-dev
+
+# Disable mosquitto persistance
+sudo sed -i "s/^persistence true/persistence false/" /etc/mosquitto/mosquitto.conf
+# append line: allow_anonymous false
+sudo sed -i -n '/allow_anonymous false/!p;$a allow_anonymous false' /etc/mosquitto/mosquitto.conf
+# append line: password_file /etc/mosquitto/passwd
+sudo sed -i -n '/password_file \/etc\/mosquitto\/passwd/!p;$a password_file \/etc\/mosquitto\/passwd' /etc/mosquitto/mosquitto.conf
+# append line: log_type error
+sudo sed -i -n '/log_type error/!p;$a log_type error' /etc/mosquitto/mosquitto.conf
+
+mqtt_user=emonpi
+mqtt_password=emonpimqtt2016
+# Create mosquitto password file
+sudo touch /etc/mosquitto/passwd
+sudo mosquitto_passwd -b /etc/mosquitto/passwd $mqtt_user $mqtt_password
+
+echo "-------------------------------------------------------------"
+echo "Mosquitto Client installation and configuration"
+echo "-------------------------------------------------------------"
+sudo apt-get install -y libmosquitto-dev
+printf "\n" | sudo pecl install Mosquitto-beta
+echo "-------------------------------------------------------------"
+# Add mosquitto to php mods available
+PHP_VER=$(php -v | head -n 1 | cut -d " " -f 2 | cut -f1-2 -d"." )
+printf "extension=mosquitto.so" | sudo tee /etc/php/$PHP_VER/mods-available/mosquitto.ini 1>&2
+sudo phpenmod mosquitto
