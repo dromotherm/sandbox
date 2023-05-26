2532a529f50ebfc338ca5d8aa1251623f62f174a
Alexandre CUER <alexandrep.cuer@gmail.com>
2023-05-26 18:46:24 +0200

    Update emoncms_stack_jetson_nano.sh

--- _HARDWARE_jetson/emoncms_stack_jetson_nano.sh
+++ _HARDWARE_jetson/emoncms_stack_jetson_nano.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 # install emoncms stack on jetson nano with php7.4
 
+# give access to serial port
+sudo usermod -a -G dialout $USER
+
+# do not upgrade kernel
+dpkg -S /boot/Image
+sudo apt-mark hold nvidia-l4t-kernel nvidia-l4t-kernel-dtbs nvidia-l4t-kernel-headers nvidia-l4t-bootloader
+
+sudo apt-get install -y git build-essential python3-pip python3-dev
+python3 -m pip install pip --upgrade
+python3 -m pip install pyserial
+
 sudo git config --system --replace-all safe.directory '*'
 
 sudo echo $USER' ALL=(ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/$USER && sudo chmod 0440 /etc/sudoers.d/$USER
@@ -15,14 +26,12 @@ make osupdate
 # install dependancies
 read -rsn1 -p"Press any key to install apache";echo
 make apache
-read -rsn1 -p"Press any key to install php";echo
+read -rsn1 -p"Press any key to install mariadb";echo
 make mysql
 read -rsn1 -p"Press any key to install php 7.4";echo
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
 sudo apt install php7.4
-sudo apt-get install -y libapache2-mod-php
-sudo apt-get install -y php-gd php-pear php-common
 sudo apt install php7.4-curl php7.4-mbstring php7.4-xml php7.4-igbinary php7.4-dev php7.4-mysql
 read -rsn1 -p"Press any key to install redis";echo
 make redis
