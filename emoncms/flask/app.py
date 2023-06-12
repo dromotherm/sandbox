09d0246634e7f3a27bd76d555bd286ac7013313b
Alexandre CUER <alexandrep.cuer@gmail.com>
2023-06-12 17:52:21 +0200

    Update app.py

--- emoncms/flask/app.py
+++ emoncms/flask/app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 
 rco = redis.Redis(host="localhost", port=6379, db=0)
 
-server_url = "http://192.168.1.25"
+server_url = "http://emoncms.ddns.net"
 server_port = 5000
-docker_image = "alexjunk/emoncms:0.0.2" 
+docker_image = "alexjunk/emoncms:0.0.3"
 
 def exec_shell_command(cmd):
     """execute a shell command"""
@@ -29,26 +29,61 @@ def get_free_port():
             rco.sadd("ports", port_nb)
             return str(port_nb)
 
+@app.route("/")
+def home():
+    content = f'Vous en avez marre d\'excel'
+    content = f'{content}<br>Vous n\'êtes pas sur de vouloir développer vos scripts de dataviz sous python'
+    content = f'{content}<br>Vous pouvez lancer votre application de dataviz ici :'
+    content = f'{content}<br><a href={server_url}/try/start>START APP</a>'
+    return content
+
 @app.route("/start")
 def start():
     nbp = get_free_port()
     cmd = [f'docker run -d -p{nbp}:80 {docker_image}']
     token = exec_shell_command(cmd)
     short_token = token[:13].decode()
-    nb_used_ports = rco.scard("ports")
-    del_route = f'{server_url}:{server_port}/delete/{nbp}/{short_token}'
+    del_route = f'{server_url}/try/delete/{nbp}/{short_token}'
     app_url = f'{server_url}:{nbp}'
-    content = f'On vous a attribué le port {nbp} pour y démarrer une application'
-    content = f'{content}<br>Il y a {nb_used_ports} ports utilisés dont celui qui vous a été attribué'
+    content = f'BRAVO : on vous a attribué le port {nbp} pour y démarrer une application'
+    content = f'{content}<br>'
+    content = f'{content}<br>1) Commencez par télécharger des données :'
+    content = f'{content}<br><a target=_blank href=http://emoncms.ddns.net/emoncms-bloch-2021.tar.gz>datas collège Marc Bloch</a>'
+    content = f'{content}<br>l\'archive se téléchargera dans le dossier téléchargements de votre ordinateur'
+    content = f'{content}<br>'
+    content = f'{content}<br>2) Lancer ensuite l\'application, créez vous un compte'
+    content = f'{content}, injectez l\'archive en allant dans backup > import archive, puis allez dans graph pour les visualiser'
+    content = f'{content}<br> ATTENTION LES DATAS SONT VIELLES, IL VOUS FAUDRA REMONTER DANS LE TEMPS'
     content = f'{content}<br><a target=_blank href={app_url}>ouvrir app</a>'
+    content = f'{content}<br>'
+    content = f'{content}<br>3) Tout ceci est gratuit alors éteignez l\'application avant de partir' 
     content = f'{content}<br><a href={del_route}>supprimer app et rendre le port</a>'
+    content = f'{content}<br>'
+    content = f'{content}<br>Si vous voulez un accès persistant, contactez nous !'
     content = f'{content}<br><script type="text/javascript">window.onbeforeunload = () => fetch({del_route});</script>'
+    return content
+
+@app.route("/list")
+def list():
+    nb_used_ports = rco.scard("ports")
+    cmd = ["docker container ls"]
+    containers = exec_shell_command(cmd)
+    containers = containers.decode().replace("\n","<br>")
+    content = f'Il y a {nb_used_ports} port(s) utilisé(s)'
+    content = f'{content}<br>{containers}'
     return content
 
 @app.route("/who")
 def who():
     cmd = ['whoami']
-    return exec_shell_command(cmd) 
+    return exec_shell_command(cmd)
+
+@app.route("/clear")
+def clear():
+    rco.delete("ports")
+    cmd = ["docker ps -aq | xargs docker stop | xargs docker rm"]
+    exec_shell_command(cmd)
+    return 'done'
 
 @app.route("/delete/<port>/<token>")
 def delete(port, token):
