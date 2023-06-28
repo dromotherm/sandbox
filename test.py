899ae36cf2a5a2075ff0c6b99543da171c1be072
Alexandre CUER <alexandrep.cuer@gmail.com>
2023-06-28 18:25:19 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,20 @@
 """
 ceci est un fichier de test
 """
-print("hello planet earth !")
+import sys
 import matplotlib.pyplot as plt
 import numpy as np
-import sys
-from contextlib import closing
-from urllib.request import urlopen
 
-a=sys.argv[0]
+print("hello planet earth !")
+
+a = sys.argv[0]
 print(a)
 
 
 for arg in sys.argv:
     print(arg)
 
-T=np.ones(500)*12
+T = np.ones(500)*12
 
 plt.subplot(111)
 plt.plot(T)
